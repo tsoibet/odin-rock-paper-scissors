7b8da5b956bd5b87cf5c5e09a4db2f6452173019
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-03-31 23:22:26 +0900

    Make the game work on webpage ui instead of console

--- external.js
+++ external.js
@@ -1,4 +1,10 @@
-// Begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+let playerScore = 0;
+let computerScore = 0;
+
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
 
 function computerPlay() {
     let num = Math.random()*100;
@@ -7,8 +13,6 @@ function computerPlay() {
     else return 'Scissors';
 }
 
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock".
-
 function playRound(playerSelection, computerSelection) {
     let roundMessage = 'Something went wrong...';
     playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
@@ -16,42 +20,35 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'Rock' && computerSelection == 'Scissors' || 
         playerSelection == 'Paper' && computerSelection == 'Rock' || 
         playerSelection == 'Scissors' && computerSelection == 'Paper') {
-            playerScore += 1;
-            roundMessage = "You Win! " + playerSelection + " beats " + computerSelection + ".";
+            playerScore++;
+            roundMessage = `You Win! ${playerSelection} beats ${computerSelection}.`;
+
         }
     else  if (playerSelection == 'Rock' && computerSelection == 'Paper' || 
               playerSelection == 'Paper' && computerSelection == 'Scissors' || 
               playerSelection == 'Scissors' && computerSelection == 'Rock') {
-                    computerScore += 1;
-                    roundMessage = "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+                computerScore++;
+                roundMessage = `You Lose! ${computerSelection} beats ${playerSelection}.`;
         }
-    else roundMessage = "Draw!"
+    else roundMessage = `Draw!`;
 
-    console.log(roundMessage);
-    return roundMessage;
+
+    scoreDiv.textContent = `You scored ${playerScore} and Computer scored ${computerScore}.`;
+    messageDiv.textContent = roundMessage;
+
+    if (playerScore == 5 || computerScore == 5) {
+        messageDiv.textContent = (playerScore > computerScore)? "YOU ARE THE FINAL WINNER!":"COMPUTER IS THE FINAL WINNER!";
+        resetScore();
     }
-    
-// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    let winner;
-    let finalMessage = 'Something went wrong...';
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper ot Scissors?");
-        playRound(playerSelection, computerPlay());
-        console.log("You scored " + playerScore + " and Computer scored " + computerScore + ".")
+    return;
     }
-    if (playerScore > computerScore) finalMessage = "YOU WIN!";
-    else if (playerScore < computerScore) finalMessage = "COMPUTER WINS!";
-    else finalMessage = "DRAW! Play again!";
 
-    console.log(finalMessage);
-    return finalMessage;
-}
-
-let playerScore = 0;
-let computerScore = 0;
-game();
+const messageDiv = document.querySelector('#result');
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+    });
+});
+const scoreDiv = document.querySelector('#score');
